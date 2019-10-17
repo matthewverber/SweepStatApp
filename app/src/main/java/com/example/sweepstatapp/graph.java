780c0dfac542bae4369e29a885278ad70521fa38
Zhizhou <yiwk321@live.unc.edu>
2019-10-17 16:40:34 -0400

    Changed addEntry(DataPoint) to public Allow drawing a second series from higher x values to lower x values

--- app/src/main/java/com/example/sweepstatapp/graph.java
+++ app/src/main/java/com/example/sweepstatapp/graph.java
@@ -1,24 +1,26 @@
 package com.example.sweepstatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import android.os.Bundle;
 import com.jjoe64.graphview.GraphView;
 import android.view.View;
+import java.util.ArrayList;
 
-import java.util.Random;
 
 public class graph extends AppCompatActivity {
-    private LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
+    private LineGraphSeries<DataPoint> forwardSeries = new LineGraphSeries<>();
+    private LineGraphSeries<DataPoint> backwardSeries = new LineGraphSeries<>();
+    private int numberOfPoints = 100;
+    private ArrayList<DataPoint> backwardData = new ArrayList<>();
     private int x = 0;
-    private int numberOfPoints = 500;
     private double offset = 0;
     private long interval = 50;
     private GraphView graph = null;
     private Viewport viewport = null;
+    private DataPoint dataPoint = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +45,7 @@ public class graph extends AppCompatActivity {
             viewport.setMaxX(numberOfPoints*0.1);
             graph.removeAllSeries();
             generateFakeData();
-            graph.addSeries(series);
+            graph.addSeries(forwardSeries);
         }
         if (view.getId() == R.id.realTime){
             offset = Math.random();
@@ -51,8 +53,11 @@ public class graph extends AppCompatActivity {
 //            numberOfPoints = 500;
 //            interval = 100;
             graph.removeAllSeries();
-            series = new LineGraphSeries<>();
-            graph.addSeries(series);
+            forwardSeries = new LineGraphSeries<>();
+            backwardData = new ArrayList<>();
+            backwardSeries = new LineGraphSeries<>();
+            graph.addSeries(forwardSeries);
+            graph.addSeries(backwardSeries);
             viewport.setMaxX(numberOfPoints*0.1);
             graphInRealTime();
         }
@@ -62,47 +67,55 @@ public class graph extends AppCompatActivity {
         return new DataPoint(0.1*x,Math.sin(0.1*x+offset));
     }
 
+    protected DataPoint generateFakeDataPointReverse(){
+        return new DataPoint(0.1*x,-1*Math.sin(0.1*x));
+    }
+
     protected void generateFakeData(){
         DataPoint[] fakeData = new DataPoint[numberOfPoints];
         for (int i = 0; i < numberOfPoints; i++, x++){
             fakeData[i] = generateFakeDataPoint();
         }
-        series = new LineGraphSeries<>(fakeData);
+        forwardSeries = new LineGraphSeries<>(fakeData);
     }
 
     protected void graphInRealTime(){
         new Thread(new Runnable() {
             @Override
             public void run() {
-                x = 0;
                 try {
+                    x = 0;
                     Thread.sleep(100);
-                } catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-               x = 0;
-                for (int i = 0; i < numberOfPoints; i++, x++){
-                    if (i != x){
-                        return;
-                    }
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            addEntry(generateFakeDataPoint());
+                    x = 0;
+                    for (int i = 0; i < numberOfPoints; i++, x++) {
+                        if (i != x) {
+                            return;
                         }
-                    });
-                    try {
+                        addEntry(generateFakeDataPoint());
                         Thread.sleep(interval);
-                    } catch (InterruptedException e){
-                        e.printStackTrace();
                     }
-
+                    x = numberOfPoints - 2;
+                    for (int i = x; i >= 0; i--, x--) {
+                        if (i != x) {
+                            return;
+                        }
+                        addEntry(generateFakeDataPointReverse());
+                        Thread.sleep(interval);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         }).start();
     }
 
-    protected void addEntry(DataPoint dataPoint){
-        series.appendData(dataPoint, false, numberOfPoints);
+    public void addEntry(DataPoint data){
+        this.dataPoint = data;
+        if(dataPoint.getX() < forwardSeries.getHighestValueX()) {
+            backwardData.add(0,dataPoint);
+            backwardSeries.resetData(backwardData.toArray(new DataPoint[0]));
+        } else {
+            forwardSeries.appendData(dataPoint, false, numberOfPoints);
+        }
     }
 }
