3b004d4999a3c2ef9b881cdfb1ad1e0e34a5201d
AmadeusSko <amadeush@live.unc.edu>
2019-11-07 19:21:05 -0500

    Added a full set of guided setup screens per the client's direction
    Added a screen for each step of the provided app script, with text entry
    points and displays of example graphs, device setup, and other displays
    as requested by the client. Currently, these entries do not change any
    SharedPreference values; this is pending a meeting with the client team
    to determine exactly what each user selection in the guided setup will
    equate to in the SharedPreferences.
    
    Signed-off-by: AmadeusSko <amadeush@live.unc.edu>

--- app/src/main/java/com/example/sweepstatapp/GuidedSetup.java
+++ app/src/main/java/com/example/sweepstatapp/GuidedSetup.java
@@ -1,5 +1,6 @@
 package com.example.sweepstatapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,15 +16,75 @@ public class GuidedSetup extends AppCompatActivity {
         if(getSupportActionBar()!=null){
             getSupportActionBar().hide();
         }
-
     }
 
     public void onClick(View view){
-        if(view.getId() == R.id.goHome){
+        int selected = view.getId();
+        if(selected == R.id.goHome){
             this.finish();
         }
-        else if(view.getId() == R.id.nextButton){
+        else if(selected == R.id.nextButton || selected == R.id.backToScreen2){
             setContentView(R.layout.guide_second_screen);
+        }
+        else if(selected == R.id.nextButton2){
+            setContentView(R.layout.guide_third_screen);
+        }
+        else if(selected == R.id.backButton2){
+            setContentView(R.layout.guided_setup);
+        }
+        else if(selected == R.id.microelectrodeButton){
+            // Do something to indicate a selected microelectrode
+            setContentView(R.layout.guide_fourth_screen);
+        }
+        else if(selected == R.id.macroelectrodeButton){
+            // Do something to indicate a selected macroelectrode
+            setContentView(R.layout.guide_fourth_screen);
+        }
+        else if(selected == R.id.nextButtonScreen4){
+            setContentView(R.layout.guide_fifth_screen);
+        }
+        else if(selected == R.id.backButtonScreen5){
+            setContentView(R.layout.guide_fourth_screen);
+        }
+        else if(selected == R.id.nextButtonScreen5){
+            setContentView(R.layout.guide_sixth_screen);
+        }
+        else if(selected == R.id.nextButtonScreen6){
+            setContentView(R.layout.guide_seventh_screen);
+        }
+        else if(selected == R.id.backButtonScreen6){
+            setContentView(R.layout.guide_fifth_screen);
+        }
+        else if(selected == R.id.nextButtonScreen7){
+            setContentView(R.layout.guide_eighth_screen);
+        }
+        else if(selected == R.id.backButtonScreen7){
+            setContentView(R.layout.guide_sixth_screen);
+        }
+        else if(selected == R.id.nextButtonScreen8){
+            setContentView(R.layout.guide_ninth_screen);
+        }
+        else if(selected == R.id.backButtonScreen8){
+            setContentView(R.layout.guide_seventh_screen);
+        }
+        else if(selected == R.id.nextButtonScreen9){
+            setContentView(R.layout.guide_tenth_screen);
+        }
+        else if(selected == R.id.backButtonScreen9){
+            setContentView(R.layout.guide_eighth_screen);
+        }
+        else if(selected == R.id.nextButtonScreen10){
+            setContentView(R.layout.guide_final_screen);
+        }
+        else if(selected == R.id.backButtonScreen10){
+            setContentView(R.layout.guide_ninth_screen);
+        }
+        else if(selected == R.id.backButtonFinalScreen){
+            setContentView(R.layout.guide_tenth_screen);
+        }
+        else if(selected == R.id.finalizeSimple){
+            Intent goToRuntime = new Intent(this, ExperimentRuntime.class);
+            startActivity(goToRuntime);
         }
     }
 
