8d453383b40f7c69bcbcb4fa45642bd3a9b74db8
AmadeusSko <amadeush@live.unc.edu>
2019-11-20 14:02:58 -0500

    Tweaked the experimental assistant UI for consistency and functionality
    Modified some of the experimental assistant screen layouts to display
    properly and allow scrolling if necessary. Also ensured that user input
    for requested parameters is present, and did the same for the advanced
    setup. Also fixed loading of SharedPreferences across the setups and the
    experiment runtime views. Next step will be to add the user informed
    consent, to define default values, and add the "About" and "Credits"
    pages.
    
    Signed-off-by: AmadeusSko <amadeush@live.unc.edu>

--- app/src/main/java/com/example/sweepstatapp/ExperimentRuntime.java
+++ app/src/main/java/com/example/sweepstatapp/ExperimentRuntime.java
@@ -19,13 +19,15 @@ public class ExperimentRuntime extends AppCompatActivity {
     static final String INITIAL_VOLTAGE = "INITIAL VOLTAGE";
     private Graph graph = null;
     private int numOfPoints = 100;
+    SharedPreferences saved;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.experiment_runtime);
-        SharedPreferences saved = this.getSharedPreferences("com.example.sweepstatapp", Context.MODE_PRIVATE);
+        saved = this.getSharedPreferences("com.example.sweepstatapp", Context.MODE_PRIVATE);
         if(getSupportActionBar() != null){
             getSupportActionBar().hide();
         }
@@ -52,6 +54,37 @@ public class ExperimentRuntime extends AppCompatActivity {
         autoSens = false;
         finalE = false;
         auxRecord = false;
+
+
+        if(saved != null){
+            initialVoltage.setText(saved.getString(INITIAL_VOLTAGE, loadFailed));
+            highVoltage.setText(saved.getString("highVoltage", loadFailed));
+            lowVoltage.setText(saved.getString("lowVoltage", loadFailed));
+            finalVoltage.setText(saved.getString("finalVoltage", loadFailed));
+            polarity.setText(saved.getString("polarity", loadFailed));
+            scanRate.setText(saved.getString("scanRate", loadFailed));
+            sweepSegments.setText(saved.getString("scanSegments", loadFailed));
+            sampleInterval.setText(saved.getString("sampleInterval", loadFailed));
+            quietTime.setText(saved.getString("quietTime", loadFailed));
+            sensitivity.setText(saved.getString("sensitivity", loadFailed));
+            autoSens = saved.getBoolean("isAutoSens", false);
+            finalE = saved.getBoolean("isFinalE", true);
+            auxRecord = saved.getBoolean("isAuxRecording", false);
+            if(autoSens)
+                findViewById(R.id.autoSensEnabled).setVisibility(View.VISIBLE);
+            if(finalE)
+                findViewById(R.id.finalEEnabled).setVisibility(View.VISIBLE);
+            if(auxRecord)
+                findViewById(R.id.auxRecordingEnabled).setVisibility(View.VISIBLE);
+        } else {
+            Toast.makeText(this, "Failed to load saved inputs!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         if(saved != null){
             initialVoltage.setText(saved.getString(INITIAL_VOLTAGE, loadFailed));
             highVoltage.setText(saved.getString("highVoltage", loadFailed));
@@ -76,6 +109,7 @@ public class ExperimentRuntime extends AppCompatActivity {
             Toast.makeText(this, "Failed to load saved inputs!", Toast.LENGTH_SHORT).show();
         }
     }
+
 
     public void onClick(View view){
         if (view.getId() == R.id.runExperiment){
