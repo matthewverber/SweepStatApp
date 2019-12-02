a7eed137bba0c2745f64b03add1ea706738c0565
Ken Liu <yiwk321@live.unc.edu>
2019-12-02 14:14:47 -0500

    Make the graph unzoomable

--- app/src/main/java/com/example/sweepstatapp/ExperimentRuntime.java
+++ app/src/main/java/com/example/sweepstatapp/ExperimentRuntime.java
@@ -98,10 +98,10 @@ public class ExperimentRuntime extends AppCompatActivity {
 
         GraphView graphView = findViewById(R.id.graph);
         Viewport viewport = graphView.getViewport();
-        viewport.setScrollable(true);
-        viewport.setScrollableY(true);
-        viewport.setScalable(true);
-        viewport.setScalableY(true);
+//        viewport.setScrollable(true);
+//        viewport.setScrollableY(true);
+//        viewport.setScalable(true);
+//        viewport.setScalableY(true);
         viewport.setXAxisBoundsManual(true);
         viewport.setYAxisBoundsManual(true);
         viewport.setMinX(-1);
